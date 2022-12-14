ebbf975bf5d4e20ff24f5c15a60a1bf76102a459
Qandeel86 <qandeel.wasim@gmail.com>
2022-12-13 22:34:45 -0500

    Cucumber class 7

--- src/test/java/steps/AddEmployeeSteps.java
+++ src/test/java/steps/AddEmployeeSteps.java
@@ -3,6 +3,7 @@ package steps;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import utils.CommonMethods;
@@ -89,33 +90,61 @@ public class AddEmployeeSteps extends CommonMethods {
         List<Map<String, String>> empFromExcel =
                 ExcelReader.excelListIntoMap(Constants.TESTDATA_FILEPATH, sheetName);
 
+
         //it returns one map from list of maps
         Iterator<Map<String, String>> itr = empFromExcel.iterator();
-        while(itr.hasNext()){
+        while (itr.hasNext()){
             //it returns the key and value for employee from excel
             Map<String, String> mapNewEmp = itr.next();
 
             sendText(addEmployee.firstNameField, mapNewEmp.get("firstName"));
             sendText(addEmployee.middleNameField, mapNewEmp.get("middleName"));
             sendText(addEmployee.lastNameField, mapNewEmp.get("lastName"));
+            String empIdValue = addEmployee.empIdLocator.getAttribute("value");
             sendText(addEmployee.photograph, mapNewEmp.get("photograph"));
-
             if(!addEmployee.checkBox.isSelected()){
                 click(addEmployee.checkBox);
             }
-
             sendText(addEmployee.createusernameField, mapNewEmp.get("username"));
             sendText(addEmployee.createpasswordField, mapNewEmp.get("password"));
             sendText(addEmployee.confirmpasswordField, mapNewEmp.get("confirmPassword"));
-
             click(addEmployee.saveButton);
+            System.out.println("click taken on save button");
+            //verification is in home-work
+            Thread.sleep(3000);
 
-            //verification is in homework
-
+            click(dashboard.empListOption);
             Thread.sleep(2000);
+            System.out.println("click taken on emp list option");
+
+            //to search the employee, we use emp id what we captured from attribute
+            sendText(employeeList.empSearchIdField, empIdValue);
+            click(employeeList.searchButton);
+
+            //verifying the employee added from the excel file
+
+            List<WebElement> rowData =
+                    driver.findElements(By.xpath("//*[@id='resultTable']/tbody/tr"));
+
+
+            for (int i =0; i<rowData.size(); i++){
+                System.out.println("I am inside the loop and worried about josh");
+                //getting the text of every element from here and storing it into string
+                String rowText = rowData.get(i).getText();
+                System.out.println(rowText);
+
+                String expectedData = empIdValue + " " + mapNewEmp.get("firstName")
+                        + " " + mapNewEmp.get("middleName") + " " + mapNewEmp.get("lastName");
+
+                //verifying the exact details  of the employee
+                Assert.assertEquals(expectedData, rowText);
+
+            }
+
             click(dashboard.addEmployeeOption);
             Thread.sleep(2000);
         }
     }
+    }
 
-}
+
--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -9,5 +9,7 @@ public class Constants {
     public static final String TESTDATA_FILEPATH =
             System.getProperty("user.dir")+"/src/test/resources/testdata/batch14excel.xlsx";
 
+    public static final String SCREENSHOT_FILEPATH =   System.getProperty("user.dir") + "/screenshots/";
+
 
 }
